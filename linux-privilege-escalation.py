cb090dfaa27831e0c0feaa5d5eb6655986def695
Kelly online <kellyonline2016@gmail.com>
2021-12-22 10:54:23 +0100

    added comments and compile path

--- linux-privilege-escalation.py
+++ linux-privilege-escalation.py
@@ -1,1 +1,16 @@
 
+#!/usr/bin/env/python3
+
+#####################################################################
+#																	#
+#		A simple automation tool for Linux Privilege Escalation	#
+#																	#
+# 		Features:												#
+#				* OS, Release, and Kernel information gathering     #
+#				* Root service check                                #
+#				* SUID-GUID check ('can this user sudo anything?')  #
+#				* Check for SUID-GUID abusable binaries             #
+#																	#
+#####################################################################
+
+
